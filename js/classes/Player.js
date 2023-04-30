ccc5290235841206ea0af538ac67905014cd5910
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-04-29 21:17:31 -0400

    Created basic player class

--- js/classes/Player.js
+++
@@ -1,5 +0,0 @@
-export default class Player {
-  constructor(name) {
-    this.name = name
-  }
-}
---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import { Player } from "./classes/index.js";
+
+const playerOne: Player = new Player('Player 1')
