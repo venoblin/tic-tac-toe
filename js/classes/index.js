83b21c39acddbf9fe0836bcae11c4630918ff461
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-04-30 01:19:52 -0400

    Made name property private

--- js/classes/index.js
+++ js/classes/index.js
@@ -9,12 +9,14 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _Screen_instances, _Screen_layout, _Screen_showAlert, _Screen_startHandler;
+var _Player_name, _Screen_instances, _Screen_layout, _Screen_showAlert, _Screen_startHandler;
 export class Player {
     constructor(name) {
-        this.name = name;
+        _Player_name.set(this, void 0);
+        __classPrivateFieldSet(this, _Player_name, name, "f");
     }
 }
+_Player_name = new WeakMap();
 export class Screen {
     constructor(options) {
         _Screen_instances.add(this);
--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,8 +1,8 @@
 export class Player {
-  name: string
+  #name: string
 
   constructor(name: string) {
-    this.name = name
+    this.#name = name
   }
 }
 
