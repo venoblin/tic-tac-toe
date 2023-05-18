9a1cb2503c97dfaba516e9396a75cd837b61fd0e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-05-17 23:48:30 -0400

    Removed unnecessary import

--- src/classes/Board.ts
+++ src/classes/Board.ts
@@ -1,5 +1,4 @@
 import DimensionalArray from "./DimensionalArray.js"
-import DOMElement from "./DOMElement.js"
 import { BoardInfo } from "../types"
 
 export default class Board extends DimensionalArray<BoardInfo> {
